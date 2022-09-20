5fa51f7dc1b9cd1cb292be0edc7948005adfbc76
Eli <+>
2022-09-20 09:40:16 -0400

    commit tree b44578058105cfcc7f1d02c371791f786e642742

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    # validate :title_is_clickbaity
+
+    # def title_is_clickbaity
+    #     unless title.match?("Won't Believe") || title.match?("Secret") || title.match?("Top") || title.match?("Guess")
+    #         errors.add(:title, "not clickbaity")
+    #     end
+    # end
+    
 end
